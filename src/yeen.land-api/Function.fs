5deb5e6fef575f06e366c4bb33f0ddc675a7754d
Jlobblet <jlobblet-github@gmx.co.uk>
2021-05-02 20:58:21 +0100

    Allow api access to a particular item

--- src/yeen.land-api/Function.fs
+++ src/yeen.land-api/Function.fs
@@ -12,26 +12,49 @@ open yeenland.Utils
 open yeenland.yeenland
 
 module Function =
-    let private GenerateResponse (url: string) =
-        let body = [ "url", url ] |> ToDictionary
-
+    let private GenerateResponse (url: string option) =
         let headers =
             [ ("Content-Type", "application/json") ]
             |> ToDictionary
 
+        let ``404`` =
+            let body = [ "url", "" ] |> ToDictionary
+            let statusCode = 404
+            body, statusCode
+
+        let body, statusCode =
+            url
+            |> Option.map (fun u ->
+                let body = [ "url", u ] |> ToDictionary
+                let statusCode = 200
+                body, statusCode)
+            |> Option.defaultValue ``404``
+
         async {
-            return APIGatewayProxyResponse(Body = JsonConvert.SerializeObject body, StatusCode = 200, Headers = headers)
+            return
+                APIGatewayProxyResponse
+                    (Body = JsonConvert.SerializeObject body, StatusCode = statusCode, Headers = headers)
         }
         |> Async.StartAsTask
 
-    let FunctionHandler (_: APIGatewayProxyRequest) (_: ILambdaContext) =
+    let FunctionHandler (request: APIGatewayProxyRequest) (_: ILambdaContext) =
         let services =
             new Service(RegionEndpoint.EUWest2) :> IServices
 
-        GetRandomRecord()
-        |> Reader.bind GetRecordUrl
-        |> Reader.run
-        <| services
+        let pathParameters =
+            request.PathParameters
+            |> Seq.map (fun kvp -> kvp.Key, kvp.Value)
+            |> Map.ofSeq
+
+        pathParameters
+        |> Map.tryFind "id"
+        |> Option.bind TryParseUInt64
+        |> Option.map (
+            TryGetRecordFromHash
+            >> Reader.bind (Option.fold (fun _ -> Reader.Return<_, _>) (GetRandomRecord()))
+            >> Reader.bind GetRecordUrl
+            >> Reader.run
+            >> fun f -> services |> f)
         |> GenerateResponse
 
     [<assembly: LambdaSerializer(typeof<DefaultLambdaJsonSerializer>)>]
